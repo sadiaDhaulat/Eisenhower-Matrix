2cbaafff6b9c2d3a1d38d1a18261b079240fed2f
USER <sadiadhaulat2003@gmail.com>
2025-08-06 10:45:52 +0600

    pushing again

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2112043106"
+    serviceWorkerVersion: "1521078741"
   }
 });
